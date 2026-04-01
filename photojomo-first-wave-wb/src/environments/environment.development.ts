7f1991280effee837af5921f511597b25e333000
paarsar-doola <al@fountainplatform.com>
2026-04-01 12:52:09 -0400

    feat:creating dev site

---
+++ photojomo-first-wave-wb/src/environments/environment.development.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  apiBaseUrl:           'https://49124qqimj.execute-api.us-east-1.amazonaws.com',
+  stripePublishableKey: 'pk_live_51KieYnHH43lCOA3EsPmwyb6R1OdY7bTLgaUcGjMKqw8ilaJxjYiTnRfr2gmh3x1oGj0YTdy8BTWAupMEiG03v3gt00r4SaK4U9',
+  paypalClientId:       'Ac0c1bMrzl8KoDao4chbt0nouRVKfJsbwrevNs1wJLuVAt9P6CZrQswwAFMK11gk3d8dPkTSorZA1Ore',
+};
